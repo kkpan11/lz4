700ac95f232310dfc5968b506ba498a2dc2d8dc3
Yann Collet <yann.collet.73@gmail.com>
2025-05-23 23:33:13 -0700

    minor refactor of LZ4F_write() in lz4file.c

--- lib/lz4file.c
+++ lib/lz4file.c
@@ -303,34 +303,33 @@ LZ4F_errorCode_t LZ4F_writeOpen(LZ4_writeFile_t** lz4fWrite, FILE* fp, const LZ4
 size_t LZ4F_write(LZ4_writeFile_t* lz4fWrite, const void* buf, size_t size)
 {
   const LZ4_byte* p = (const LZ4_byte*)buf;
-  size_t remain = size;
-  size_t chunk;
-  size_t ret;
+  size_t remainingBytes = size;
 
+  /* Validate parameters */
   if (lz4fWrite == NULL || buf == NULL)
     RETURN_ERROR(parameter_null);
-  while (remain) {
-    if (remain > lz4fWrite->maxWriteSize)
-      chunk = lz4fWrite->maxWriteSize;
-    else
-      chunk = remain;
 
-    ret = LZ4F_compressUpdate(lz4fWrite->cctxPtr,
+  while (remainingBytes) {
+    size_t const chunkSize = (remainingBytes > lz4fWrite->maxWriteSize) ? lz4fWrite->maxWriteSize : remainingBytes;
+
+    /* Compress and write chunk */
+    size_t cSize = LZ4F_compressUpdate(lz4fWrite->cctxPtr,
                               lz4fWrite->dstBuf, lz4fWrite->dstBufMaxSize,
-                              p, chunk,
+                              p, chunkSize,
                               NULL);
-    if (LZ4F_isError(ret)) {
-      lz4fWrite->errCode = ret;
-      return ret;
+    if (LZ4F_isError(cSize)) {
+      lz4fWrite->errCode = cSize;
+      return cSize;
     }
 
-    if (ret != fwrite(lz4fWrite->dstBuf, 1, ret, lz4fWrite->fp)) {
+    if (cSize != fwrite(lz4fWrite->dstBuf, 1, cSize, lz4fWrite->fp)) {
       lz4fWrite->errCode = returnErrorCode(LZ4F_ERROR_io_write);
       RETURN_ERROR(io_write);
     }
 
-    p += chunk;
-    remain -= chunk;
+    /* Update positions */
+    p += chunkSize;
+    remainingBytes -= chunkSize;
   }
 
   return size;
@@ -349,7 +348,7 @@ LZ4F_errorCode_t LZ4F_writeClose(LZ4_writeFile_t* lz4fWrite)
                             lz4fWrite->dstBuf, lz4fWrite->dstBufMaxSize,
                             NULL);
     if (LZ4F_isError(ret)) {
-      goto out;
+      goto cleanup;
     }
 
     if (ret != fwrite(lz4fWrite->dstBuf, 1, ret, lz4fWrite->fp)) {
@@ -357,7 +356,7 @@ LZ4F_errorCode_t LZ4F_writeClose(LZ4_writeFile_t* lz4fWrite)
     }
   }
 
-out:
+cleanup:
   freeWriteFileResources(lz4fWrite);
   return ret;
 }
